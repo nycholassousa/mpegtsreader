19c3a8358f716a943f5d56f9a97037d659b91823
Nycholas de Sousa <nycholas@live.com>
2016-06-27 21:31:53 -0300

    Minor fixes

--- mpegtsreader/Main.cpp
+++ mpegtsreader/Main.cpp
@@ -13,26 +13,18 @@ INFOS:
 	da tabela PAT na página 43; e da tabela PMT na página 46.
 2.  Correções e verificações dos valores foram feitos através do software disponibilizado em:
 	http://www.pjdaniel.org.uk/mpeg/.
-3.	Perguntar: Ocorre PMT e PAT ao mesmo tempo?
-
-TODO:
-1.  Corrigir erro do program_map_PID/netword_PID da tabela PAT, 
-	está imprimindo um valor incorreto (rever os bytes/bits) - EM ANDAMENTO
-2.  Inserir tabela PMT - EM ANDAMENTO
 */
 
 int main(int argc, char ** argv) {
 	//Usando o programa através de argumentos
-	/*
 	if (argc != 2) {
 		std::cerr << "Uso: mpegtsreader <video.extensao>" << std::endl;
 		return EXIT_FAILURE;
 	}
-	*/
 
-	//Parser* tsParser = new Parser(argv[1]);
-	argv[1] = "video.ts";
-	Parser *tsParser = new Parser("C:\\Users\\nycho\\Documents\\GitHub\\mpegtsreader\\Debug\\video.ts");
+	Parser* tsParser = new Parser(argv[1]);
+	//argv[1] = "video.ts";
+	//Parser *tsParser = new Parser("C:\\Users\\nycho\\Documents\\GitHub\\mpegtsreader\\Debug\\video.ts");
 
 	//Para evitar erros e devido o append ao abrir o arquivo, o packets.txt antigo sempre será deletado ao executar o programa
 	remove("packets.txt");
